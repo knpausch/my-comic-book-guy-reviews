5f57353caaf8ce49a357c82b9dc1d2aea7a9825c
Keyanna Kiko Pausch <knpausch@gmail.com>
2023-01-11 14:24:36 -0700

    added rankInterface and EpisodeDetail components

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,24 +4,43 @@ import './App.css'
 import Banner from '../Banner/Banner'
 import RankResult from "../RankResult/RankResult";
 import EpisodeBank from "../EpisodeBank/EpisodeBank";
+import EpisodeDetail from "../EpisodeDetail/EpisodeDetail";
+import RankInterface from "../RankInterface/RankInterface";
 
-class App extends Component{
-  constructor(){
+class App extends Component {
+  constructor() {
     super()
-    this.state={
+    this.state = {
       episodes: []
     }
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <main>
         <Banner />
-        <RankResult />
-        <EpisodeBank />
+        {/* <RankResult />
+        <EpisodeBank /> */}
+        <EpisodeDetail />
+        <RankInterface />
       </main>
     )
   }
 }
+
+{/* <Switch>
+  <Route exact path="/" render={() => {
+    return this.state.error ? <h2>{this.state.error}</h2> :
+      <div>
+        <RatingFilter filterByRating={this.filterByRating} />
+        <Movies movies={this.state.filteredMovies} loading={this.state.loading} seeMovieDetails={this.seeMovieDetails} />
+      </div>
+  }} />
+  <Route exact path="/movie/:id" render={() => {
+    return this.state.error ? <h2>{this.state.error}</h2> :
+      <MovieDetails singleMovie={this.state.singleMovie} displayHome={this.displayHome} />
+  }} />
+  <Route component={Status404} />
+</Switch> */}
 
 export default App
---
+++ src/Components/EpisodeDetail/EpisodeDetail.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './EpisodeDetail.css'
+
+const EpisodeDetail = () => {
+    return(
+        <div>
+            <h4>Episode's Details</h4>
+        </div>
+    )
+}
+
+export default EpisodeDetail
