4c928c18bfe623c23ee03b537f5c1827d4614305
Keyanna Kiko Pausch <knpausch@gmail.com>
2023-01-12 10:58:36 -0700

    WIP: adding css styling to episodeDetail and rankInterface components

---
+++ src/Components/EpisodeSheet/EpisodeSheet.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './EpisodeSheet.css'
+
+const EpisodeSheet = () => {
+    return(
+        <div className='episode-sheet'>
+            <h4>Name: Duffless</h4>
+            <h5>Season: 1</h5>
+            <h6>Episoe: 5</h6>
+        </div>
+    )
+}
+
+export default EpisodeSheet
--- src/Components/RankInterface/RankInterface.js
+++ src/Components/RankInterface/RankInterface.js
@@ -1,10 +1,16 @@
 import React from "react";
 import './RankInterface.css'
+const krustyButtonImg = require('../../images/krusty.png')
+const lisaButtonImg = require('../../images/lisa.png')
+const lennyButtonImg = require('../../images/lenny.png')
 
 const RankInterface = () => {
     return(
-        <div>
+        <div className='rank-interface-container'>
             <h5>Rank: 1, 2, or 3</h5>
+            <img src={krustyButtonImg}/>
+            <img src={lisaButtonImg}/>
+            <img src={lennyButtonImg}/>
         </div>
     )
 }
