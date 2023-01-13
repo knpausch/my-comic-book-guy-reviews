1f3c24899f8a8236ef3ebfbfc335ad6ccaa203e6
Keyanna Kiko Pausch <knpausch@gmail.com>
2023-01-12 18:03:43 -0700

    imported useparams and can grab whatever id the user types in the url to find corresponding episode

--- src/Components/EpisodeDetail/EpisodeDetail.js
+++ src/Components/EpisodeDetail/EpisodeDetail.js
@@ -1,13 +1,24 @@
 import React from "react";
 import './EpisodeDetail.css'
 import EpisodeSheet from "../EpisodeSheet/EpisodeSheet";
+import { useParams } from 'react-router-dom';
+
 const tvImg = require('../../images/tv.png')
 const turnBackImg = require('../../images/turn-back.png')
 
-const EpisodeDetail = () => {
+const EpisodeDetail = ({ episodes }) => {
+    let id = parseInt(useParams().id);
+    console.log("You entered: ", id)
+
+    const foundEpisode = episodes.find(episode => {
+        return episode.id === id
+    })
+
+    console.log("Found Episode: ", foundEpisode)
+
     return (
         <div className='episode-details-and-button-container'>
-            <button className='go-back-button'>Go Back <img src={turnBackImg} className='go-back-img'/></button>
+            <button className='go-back-button'>Go Back <img src={turnBackImg} className='go-back-img' /></button>
             <div className='episode-details-container'>
                 <EpisodeSheet />
                 <img src={tvImg} />
