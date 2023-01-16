dbae563db12c23de4a824d4bc199c58085dd43f0
Keyanna Kiko Pausch <knpausch@gmail.com>
2023-01-15 20:22:58 -0700

    refactored errorHandling tests for DRY code

--- cypress/e2e/errorHandling.cy.js
+++ cypress/e2e/errorHandling.cy.js
@@ -1,12 +1,12 @@
-describe("Error handling for 404 & 500 Error", () => {
-    it("Should display error message and redirect button if user enters a bad URL", () => {
+describe("Error handling for 404 errors", () => {
+    beforeEach(() => {
         cy.intercept('https://api.sampleapis.com/simpsons/episodes', {
             method: 'GET',
             fixture: '../fixtures/getEpisodes.json'
         })
-
         cy.visit("http://localhost:3000/banana")
-
+    })
+    it("Should display error message and redirect button if user enters a bad URL", () => {
         cy.get('.banner-img').should('be.visible')
         cy.get('.bad-url-text').should('contain', 'D\'oh! URL not found')
         cy.get('.redirect-button').should('contain', 'Go Back Home')
@@ -14,13 +14,6 @@ describe("Error handling for 404 & 500 Error", () => {
     })
 
     it("Should return user back to home after clicking on 'Go Back Home' button", () => {
-        cy.intercept('https://api.sampleapis.com/simpsons/episodes', {
-            method: 'GET',
-            fixture: '../fixtures/getEpisodes.json'
-        })
-
-        cy.visit("http://localhost:3000/banana")
-
         cy.get('.redirect-button').click()
 
         cy.get('.banner-img').should('be.visible')
@@ -32,13 +25,16 @@ describe("Error handling for 404 & 500 Error", () => {
         cy.get('.thumbnail-img').eq(2).should('have.attr', 'src', 'https://images-na.ssl-images-amazon.com/images/M/MV5BNzMwODQzMTQ4N15BMl5BanBnXkFtZTgwODU1NjQ2MjE@._V1_UX200_CR0,0,200,112_AL_.jpg')
         cy.get('.thumbnail-img').eq(3).should('have.attr', 'src', 'https://images-na.ssl-images-amazon.com/images/M/MV5BNjkzMjAzMzc4NF5BMl5BanBnXkFtZTgwNDY2NTQ2MjE@._V1_UX200_CR0,0,200,112_AL_.jpg')
     })
+})
 
-    it("Should display error message if API server went down while user is on home page", () => {
+describe('Error handling for 500 errors', () => {
+    beforeEach(() => {
         cy.intercept('https://api.sampleapis.com/simpsons/episodes', {
             method: 'GET',
             statusCode: 500
         })
-
+    })
+    it("Should display error message if API server went down while user is on home page", () => {
         cy.visit("http://localhost:3000/")
 
         cy.get('.banner-img').should('be.visible')
@@ -46,14 +42,9 @@ describe("Error handling for 404 & 500 Error", () => {
         cy.get('.sad-homer-img').should('have.attr', 'src', '/static/media/sad-homer.65f7e167de109219d207.png')
     })
 
-    it.only("Should display error message if API server went down while user is on detail page", () => {
-        cy.intercept('https://api.sampleapis.com/simpsons/episodes', {
-            method: 'GET',
-            statusCode: 500
-        })
-
+    it("Should display error message if API server went down while user is on detail page", () => {
         cy.visit("http://localhost:3000/episodeDetails/1")
-        
+
         cy.get('.banner-img').should('be.visible')
         cy.get('.server-down-text').should('contain', 'D\'oh! Something went wrong. Please try again later')
         cy.get('.sad-homer-img').should('have.attr', 'src', '/static/media/sad-homer.65f7e167de109219d207.png')
